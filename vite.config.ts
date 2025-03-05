f32b0203367d6499cdb6e02600462429bf908f30
yosbany <ytejas.86@gmail.com>
2025-03-05 01:23:04 -0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sgo-app/',
+  base: '/nrd-sgo/',
   server: {
     port: 3000,
     open: true
   },
   css: {
     postcss: './postcss.config.cjs'
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
