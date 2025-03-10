4e5baf4c0feacb6c5acbe4d6927edbee85c7830e
yosbany <ytejas.86@gmail.com>
2025-03-10 16:54:03 -0300

    chore: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/nrd-sgo/' : '/',
+  base: '/nrd-sgo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
