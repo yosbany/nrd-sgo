e1a68a411ee0f6399c388b1a95ed120e25a89b56
yosbany <ytejas.86@gmail.com>
2025-03-16 21:55:37 -0300

    fix: simplifica manejo de variables secretas en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -31,15 +31,5 @@ export default defineConfig({
       }
     }
   },
-  define: {
-    'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
-    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
-    'import.meta.env.VITE_FIREBASE_DATABASE_URL': JSON.stringify(process.env.VITE_FIREBASE_DATABASE_URL),
-    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
-    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET),
-    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
-    'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify(process.env.VITE_FIREBASE_APP_ID),
-    'import.meta.env.VITE_APP_NAME': JSON.stringify(process.env.VITE_APP_NAME),
-    'import.meta.env.VITE_APP_ENV': JSON.stringify(process.env.VITE_APP_ENV || 'production')
-  }
+  envPrefix: 'VITE_'
 })
